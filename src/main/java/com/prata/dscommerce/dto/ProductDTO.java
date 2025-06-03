93cdc3abcbe3265c57fe2e01acfa2f69facaf6fd
Gabriel Prata <ghprata7245@gmail.com>
2025-06-03 11:17:41 -0300

    ProductDTO with categories

--- src/main/java/com/prata/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/prata/dscommerce/dto/ProductDTO.java
@@ -1,9 +1,14 @@
 package com.prata.dscommerce.dto;
 
+import com.prata.dscommerce.entities.Category;
 import com.prata.dscommerce.entities.Product;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ProductDTO {
@@ -19,6 +24,8 @@ public class ProductDTO {
     private Double price;
     private String imgUrl;
 
+    @NotEmpty(message = "Deve ter pelo menos uma categoria")
+    private List<CategoryDTO> categories = new ArrayList<>();
 
     public ProductDTO() {
     }
@@ -37,6 +44,9 @@ public class ProductDTO {
         description = entity.getDescription();
         price = entity.getPrice();
         imgUrl = entity.getImgUrl();
+        for (Category cat : entity.getCategories()) {
+            categories.add(new CategoryDTO(cat));
+        }
     }
 
     public Long getId() {
@@ -57,5 +67,9 @@ public class ProductDTO {
 
     public String getImgUrl() {
         return imgUrl;
+    }
+
+    public List<CategoryDTO> getCategories() {
+        return categories;
     }
 }
