0974ad3b39d328f2d3532bd06c8fbbb378dc0fd2
Gabriel Prata <ghprata7245@gmail.com>
2025-06-03 16:03:44 -0300

    Saving Order

--- src/main/java/com/prata/dscommerce/Services/OrderService.java
+++ src/main/java/com/prata/dscommerce/Services/OrderService.java
@@ -2,13 +2,18 @@ package com.prata.dscommerce.Services;
 
 import com.prata.dscommerce.Services.exceptios.ResourceNotFoundException;
 import com.prata.dscommerce.dto.OrderDTO;
+import com.prata.dscommerce.dto.OrderItemDTO;
 import com.prata.dscommerce.dto.ProductDTO;
-import com.prata.dscommerce.entities.Order;
-import com.prata.dscommerce.entities.Product;
+import com.prata.dscommerce.entities.*;
+import com.prata.dscommerce.repositories.OrderItemRepository;
 import com.prata.dscommerce.repositories.OrderRepository;
+import com.prata.dscommerce.repositories.ProductRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
 
 @Service
 public class OrderService {
@@ -16,9 +21,40 @@ public class OrderService {
     @Autowired
     private OrderRepository repository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
+
+    @Autowired
+    private UserService userService;
+
     @Transactional(readOnly = true)
     public OrderDTO findById(Long id) {
         Order order = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
+        return new OrderDTO(order);
+    }
+
+    @Transactional
+    public OrderDTO insert(OrderDTO dto) {
+        Order order = new Order();
+
+        order.setMoment(Instant.now());
+        order.setStatus(OrderStatus.WAITING_PAYMENT);
+
+        User user = userService.authenticated();
+        order.setClient(user);
+
+        for (OrderItemDTO itemDTO : dto.getItems()) {
+            Product product = productRepository.getReferenceById(itemDTO.getProductId());
+            OrderItem item = new OrderItem(order, product, itemDTO.getQuantity(), product.getPrice());
+            order.getItems().add(item);
+        }
+
+        repository.save(order);
+        orderItemRepository.saveAll(order.getItems());
+
         return new OrderDTO(order);
     }
 }
--- src/main/java/com/prata/dscommerce/dto/OrderDTO.java
+++ src/main/java/com/prata/dscommerce/dto/OrderDTO.java
@@ -3,6 +3,7 @@ package com.prata.dscommerce.dto;
 import com.prata.dscommerce.entities.Order;
 import com.prata.dscommerce.entities.OrderItem;
 import com.prata.dscommerce.entities.OrderStatus;
+import jakarta.validation.constraints.NotEmpty;
 
 import java.time.Instant;
 import java.util.ArrayList;
@@ -18,6 +19,7 @@ public class OrderDTO {
 
     private PaymentDTO payment;
 
+    @NotEmpty(message = "Deve ter pelo menos um item")
     private List<OrderItemDTO> items = new ArrayList<>();
 
     public OrderDTO() {
